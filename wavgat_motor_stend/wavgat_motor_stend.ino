d729e1d3b35d53946260a7a2288e0ab0ef9032f4
ssergeyev <sss999@narod.ru>
2021-03-23 09:25:50 +0600

    Motor stend, sensor configured.

--- wavgat_motor_stend/wavgat_motor_stend.ino
+++ wavgat_motor_stend/wavgat_motor_stend.ino
@@ -1,36 +1,10 @@
-/*
-  Analog Input
-
-  Demonstrates analog input by reading an analog sensor on analog pin 0 and
-  turning on and off a light emitting diode(LED) connected to digital pin 13.
-  The amount of time the LED will be on and off depends on the value obtained
-  by analogRead().
-
-  The circuit:
-  - potentiometer
-    center pin of the potentiometer to the analog input 0
-    one side pin (either one) to ground
-    the other side pin to +5V
-  - LED
-    anode (long leg) attached to digital output 13
-    cathode (short leg) attached to ground
-
-  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
-    board, the LED is optional.
-
-  created by David Cuartielles
-  modified 30 Aug 2011
-  By Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInput
-*/
 
 int sensorPin = A7;    // select the input pin for the potentiometer
 int sensorOn = 2;
 int ledPin = 13;      // select the pin for the LED
 int button = A6;
+int m_r = 9;
+int m_l =10;
 int sensorValue = 0;  // variable to store the value coming from the sensor<
 
 void setup() {
@@ -52,5 +26,6 @@ void loop() {
 */
   
   Serial.println (sensorValue);
+  Serial.println (digitalRead(button));
   delay(10);
 }
