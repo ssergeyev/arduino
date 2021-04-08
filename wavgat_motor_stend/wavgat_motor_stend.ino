e1b39eca9aa3ecd7596e2bcf734a1ab3159eb5ca
ssergeyev <sss999@narod.ru>
2021-04-08 12:03:16 +0600

    Add diffirent betwen atmega328p and LGT8f328p

--- wavgat_motor_stend/wavgat_motor_stend.ino
+++ wavgat_motor_stend/wavgat_motor_stend.ino
@@ -7,15 +7,20 @@ int irPin = 2;			// int-0 interrupt from sensor amplifier
 int m_r = 9;			// PWM motor right rotate
 int m_l =10;			// PWM motor left rotate
 int sensorValue = 0;	// variable to store the value coming from the sensor<
-unsigned long t = 0,t1 = 0;
+unsigned long t = 0,t1 = 0, t2 = 0;
 unsigned long rotate = 0;	// sensor have 4 window in sensor disk, one rotate = 4 signals
 
 void setup() {
+  // Пины D9 и D10 - 31.4 кГц
+  TCCR1A = 0b00000001;  // 8bit
+  TCCR1B = 0b00000001;  // x1 phase correct
   pinMode(ledPin, OUTPUT);
   pinMode(sensorOn, OUTPUT);
   pinMode(sensorPin, INPUT);
   pinMode(button, INPUT_PULLUP);
   pinMode(irPin, INPUT);
+  pinMode(m_l, OUTPUT);
+  pinMode(m_r, OUTPUT);
   digitalWrite(sensorOn, HIGH);
   Serial.begin(9600);
   rotate = 0;
@@ -35,6 +40,15 @@ void loop() {
     t1=t;
     Serial.println((rotate/4)*60);	// calculate rotates per minuts (RPM) and print
     rotate=0; 						// drop window counter
+  }
+  
+  if(t-t2>=100){
+    t2=t;
+    sensorValue=analogRead(sensorPin);
+    sensorValue=map(sensorValue,0,4080,255,0);    //for LGT8F32P
+    //sensorValue=map(sensorValue,0,1023,255,0);    //for Atmega328p
+    Serial.println(sensorValue);
+    analogWrite(10,sensorValue);
   }
   
 } // main
